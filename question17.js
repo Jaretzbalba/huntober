e404b261cd771a61e901f2a78a4b33bdcf01c56e
Jaret Balba <Jaretzbalba@gmail.com>
2022-12-22 11:57:28 -0800

    add question 17

---
+++ question17.js
@@ -0,0 +1,13 @@
+// 11/21/22
+// 7 kyu: Remove consecutive duplicate words
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+const removeConsecutiveDuplicates = s =>
+  s
+    .split(' ')
+    .filter((e, i, a) => e !== a[i + 1])
+    .join(' ');
